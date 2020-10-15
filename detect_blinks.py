7fae4cb765298da57f4f08fc80e48fa119e43388
enjcat <tlawltjq0110@naver.com>
2020-10-15 17:31:48 +0900

    #area = cv2.contourArea(leftEye) 넓이 구하는 코드 코드 병합과정에서 불필요한 중복 없앰

--- detect_blinks.py
+++ detect_blinks.py
@@ -58,149 +58,7 @@ def eye_aspect_ratio(eye):
 def drawPartLine(frame, part):
 	PartHull = cv2.convexHull(part)
 	cv2.drawContours(frame, [PartHull], -1, (0, 255, 0), 1)
-
-#<<<<<<< HEAD
  
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-ap.add_argument("-v", "--video", type=str, default="",
-	help="path to input video file")
-args = vars(ap.parse_args())
- 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.23
-EYE_AR_CONSEC_FRAMES = 3
-
-# initialize the frame counters and the total number of blinks
-COUNTER = 0
-TOTAL = 0
-
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-
-# grab the indexes of the facial landmarks for the left and
-# right eye, respectively
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-#other part
-(mouth_S, mouth_E) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-(inmouth_S, inmouth_E) = face_utils.FACIAL_LANDMARKS_IDXS["inner_mouth"]
-(eyebrowR_S, eyebrowR_E) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
-(eyebrowL_S, eyebrowL_E) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
-(nose_S, nose_E) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
-(jaw_S, jaw_E) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
-
-# start the video stream thread
-print("[INFO] starting video stream thread...")
-vs = FileVideoStream(args["video"]).start()
-fileStream = True
-# vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
-# fileStream = False
-time.sleep(1.0)
-
-# loop over frames from the video stream
-while True:
-	# if this is a file video stream, then we need to check if
-	# there any more frames left in the buffer to process
-	if fileStream and not vs.more():
-		break
-
-	# grab the frame from the threaded video file stream, resize
-	# it, and convert it to grayscale
-	# channels)
-	frame = vs.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-	# detect faces in the grayscale frame
-	rects = detector(gray, 0)
-
-	# loop over the face detections
-	for rect in rects:
-		# determine the facial landmarks for the face region, then
-		# convert the facial landmark (x, y)-coordinates to a NumPy
-		# array
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
-
-		# extract the left and right eye coordinates, then use the
-		# coordinates to compute the eye aspect ratio for both eyes
-		leftEye = shape[lStart:lEnd]
-		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
-		#other part
-		Mouth = shape[mouth_S:mouth_E]
-		inner_Mouth = shape[inmouth_S:inmouth_E]
-		right_Eyebrow = shape[eyebrowR_S:eyebrowR_E]
-		left_Eyebrow = shape[eyebrowL_S:eyebrowL_E]
-		Nose = shape[nose_S:nose_E]
-		Jaw = shape[jaw_S:jaw_E]
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
-
-		#area = cv2.contourArea(leftEye)
-
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-		#other part
-		drawPartLine(frame, Mouth)
-		drawPartLine(frame, inner_Mouth)
-		drawPartLine(frame, right_Eyebrow)
-		drawPartLine(frame, left_Eyebrow)
-		drawPartLine(frame, Nose)
-		drawPartLine(frame, Jaw)
-		cv2.rectangle(frame, (rect.left(), rect.top()),
-			(rect.right(), rect.bottom()), (0,0,255), 2)
-
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
-
-		# otherwise, the eye aspect ratio is not below the blink
-		# threshold
-		else:
-			# if the eyes were closed for a sufficient number of
-			# then increment the total number of blinks
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
-
-			# reset the eye frame counter
-			COUNTER = 0
-
-		# draw the total number of blinks on the frame along with
-		# the computed eye aspect ratio for the frame
-		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
- 
-	# show the frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
-#=======
-
 
 def videoReading():
 	# define two constants, one for the eye aspect ratio to indicate
@@ -355,22 +213,14 @@ def faceReading():
 				writer.writerow(([elapsed] + [item for sublist in shape for item in sublist]))
 			# compute the convex hull for the left and right eye, then
 			# visualize each of the eyes
-			leftEyeHull = cv2.convexHull(leftEye)
-			rightEyeHull = cv2.convexHull(rightEye)
-			cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-			cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-			mouthHull = cv2.convexHull(mouth)
-			cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-			innerMouthHull = cv2.convexHull(innerMouth)
-			cv2.drawContours(frame, [innerMouthHull], -1, (0, 0, 255), 1)
-			rightEyebrowHull = cv2.convexHull(rightEyebrow)
-			cv2.drawContours(frame, [rightEyebrowHull], -1, (0, 255, 0), 1)
-			leftEyebrowHull = cv2.convexHull(leftEyebrow)
-			cv2.drawContours(frame, [leftEyebrowHull], -1, (0, 255, 0), 1)
-			noseHull = cv2.convexHull(nose)
-			cv2.drawContours(frame, [noseHull], -1, (0, 255, 0), 1)
-			jawHull = cv2.convexHull(jaw)
-			cv2.drawContours(frame, [jawHull], -1, (0, 255, 0), 1)
+			drawPartLine(frame, Mouth)
+			drawPartLine(frame, inner_Mouth)
+			drawPartLine(frame, right_Eyebrow)
+			drawPartLine(frame, left_Eyebrow)
+			drawPartLine(frame, Nose)
+			drawPartLine(frame, Jaw)
+			cv2.rectangle(frame, (rect.left(), rect.top()),
+				(rect.right(), rect.bottom()), (0,0,255), 2)
 			
 			# draw the total number of blinks on the frame along with
 			# the computed eye aspect ratio for the frame
@@ -432,4 +282,3 @@ if __name__ == "__main__":
 	faceReading()
 
 	# Print the result
-#>>>>>>> main
